17a509a10917b8c1fc9acfb465a9b9160e3c4ab7
mbikal <mbikal85@gmail.com>
2026-01-08 15:50:11 +0545

    rss to json through my own code

---
+++ scraper/rsstojson.py
@@ -0,0 +1,23 @@
+import feedparser
+import json
+
+def rss_to_json(rss_url):
+    feed = feedparser.parse(rss_url)
+    items = []
+    
+    for entry in feed.entries:
+        items.append({
+            "title": entry.title,
+            "link": entry.link,
+            "description": entry.description,
+            "published": entry.published
+        })
+    data = {
+        "feed_title": feed.feed.title,
+        "items": items
+    }
+    return data
+if __name__ == "__main__":  
+    rss_url = "https://raw.githubusercontent.com/mbikal/news/main/rss/rss.xml"
+    with open("/rss/rss_feed.json", "w", encoding="utf-8") as f:
+        json.dump(rss_to_json(rss_url), f, ensure_ascii=False, indent=4)
